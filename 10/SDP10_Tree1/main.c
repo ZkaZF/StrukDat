8f6c9ceaa6a6ae88f8ec16194d444f80e22dff95
ZkaZF <azkawayasy@gmail.com>
2025-11-30 21:28:01 +0700

    melanjutkan folder 10 tree 1

--- 10/SDP10_Tree1/main.c
+++ 10/SDP10_Tree1/main.c
@@ -37,7 +37,32 @@ int main()
      printf("nilai maksimal antara 10 dan 20 adalah %d\n", max2(10, 20));
      printf("tinggi pohon: %d\n", Tinggi(B));
      printf("apakah ada elemen 'K': %s\n", SearchX(B, 'K') ? "true" : "false");
+     printf("apakah ada elemen 'Z': %s\n", SearchX(B, 'Z') ? "true" : "false");
+     UpdateX(&B, 'K', 'Z');
+     printf("setelah update K menjadi Z\n");
+     PrintPrefix(B);
+     printf("\n");
+     printf("apakah ada elemen 'Z': %s\n", SearchX(B, 'Z') ? "true" : "false");
+     printf("jumlah elemen 'T': %d\n", CountX(B, 'T'));
      printf("skew left? %s\n", IsSkewLeft(B) ? "true" : "false");
      printf("skew right? %s\n", IsSkewRight(B) ? "true" : "false");
+     printf("prefix ringkas: ");
+     PrintPrefixRingkas(B);
+     printf("\n");
+     printf("level dari elemen 'A': %d\n", LevelX(B, 'A'));
+     printf("jumlah elemen pada level 3: %d\n", CountLevel(B, 3));
+     printf("elemen pada level 3: ");
+     PrintLevel(B, 3);
+     printf("\n");
+     printf("daun terkiri: %c\n", GetDaunTerkiri(B));
+     printf("frekuensi elemen 'T': %.2f\n", FrekuensiX(B, 'T'));
+     printf("jumlah elemen vokal: %d\n", CountVocal(B));
+     PrintVocal(B);
+     printf("\n");
+     printf("jumlah elemen konsonan: %d\n", CountConsonant(B));
+     PrintConsonant(B);
+     printf("\n");
+     printf("modus : %c\n", Modus(B));
+
      return 0;
 }
--- 10/SDP10_Tree1/pohon1.c
+++ 10/SDP10_Tree1/pohon1.c
@@ -255,13 +255,40 @@ boolean SearchX(bintree P, infotype X)
 { I.S. L, X, Y terdefinisi }
 { F.S. L tetap, atau elemen bernilai X berubah menjadi Y.
 Mengganti elemen bernilai X menjadi Y}*/
-void UpdateX(bintree *P, infotype X, infotype Y);
+void UpdateX(bintree *P, infotype X, infotype Y)
+{
+    // kamus lokal
+    // algoritma
+    if (*P != NIL)
+    {
+        if (info(*P) == X)
+        {
+            info(*P) = Y;
+        }
+        UpdateX(&left(*P), X, Y);
+        UpdateX(&right(*P), X, Y);
+    }
+}
 
 /*********** SOAL TAMBAHAN, DIKERJAKAN BILA LUANG *****************/
 
 /*function CountX(P:BinTree, X:infotype) -> integer
 { Mengirimkan banyaknya node dari P yang bernilai X }*/
-int CountX(bintree P, infotype X);
+int CountX(bintree P, infotype X)
+{
+    // kamus lokal
+    int count;
+    // algoritma
+    if (P == NIL)
+    {
+        return 0;
+    }
+    else
+    {
+        count = (info(P) == X ? 1 : 0) + CountX(left(P), X) + CountX(right(P), X);
+        return count;
+    }
+}
 
 /*function IsSkewLeft (P : BinTree)-> boolean
 { Mengirim true jika P adalah pohon condong kiri } */
@@ -305,47 +332,307 @@ boolean IsSkewRight(bintree P)
 {I.S. L terdefinisi; F.S. :-}
 { menampilkan info semua elemen bintree P secara prefix linier ringkas} */
 /*contoh: A(B(( ),D),C)*/
-void PrintPrefixRingkas(bintree P);
+void PrintPrefixRingkas(bintree P)
+{
+    // kamus lokal
+
+    // algoritma
+    if (P == NIL)
+    {
+        printf("()");
+    }
+    else if (IsDaun(P))
+    {
+        printf("%c", info(P));
+    }
+    else
+    {
+        printf("%c(", info(P));
+        PrintPrefixRingkas(left(P));
+        printf(",");
+        PrintPrefixRingkas(right(P));
+        printf(")");
+    }
+}
 
 /*function LevelX(P:BinTree, X:infotype)-> integer
 { Mengirimkan level dari node X yang merupakan salah satu simpul dari pohon biner P. Akar(P) level-nya adalah 1. Pohon P tidak kosong. }*/
-int LevelX(bintree P, infotype X);
+int LevelX(bintree P, infotype X)
+{
+    // kamus lokal
+    int levelLeft, levelRight;
+    // algoritma
+    if (P == NIL)
+    {
+        return 0;
+    }
+    else if (info(P) == X)
+    {
+        return 1;
+    }
+    else
+    {
+        levelLeft = LevelX(left(P), X);
+        levelRight = LevelX(right(P), X);
+        if (levelLeft != 0)
+        {
+            return levelLeft + 1;
+        }
+        else if (levelRight != 0)
+        {
+            return levelRight + 1;
+        }
+        else
+        {
+            return 0;
+        }
+    }
+}
 
 /*function CountLevel(P:BinTree, T:integer)-> integer
 { menghitung banyaknya node pada tingkat T. }*/
-int CountLevel(bintree P, int T);
+int CountLevel(bintree P, int T)
+{
+    // kamus lokal
+    int count;
+    // algoritma
+    if (P == NIL)
+    {
+        return 0;
+    }
+    else if (T == 1)
+    {
+        return 1;
+    }
+    else
+    {
+        count = CountLevel(left(P), T - 1) + CountLevel(right(P), T - 1);
+        return count;
+    }
+}
 
 /*procedure PrintLevel( input P:bintree, input N: integer )
 {I.S. L terdefinisi; F.S. :-}
 { menampilkan info semua elemen bintree P pada generasi/level N}*/
-void PrintLevel(bintree P, int N);
+void PrintLevel(bintree P, int N)
+{
+    // kamus lokal
+
+    // algoritma
+    if (P != NIL)
+    {
+        if (N == 1)
+        {
+            printf("%c ", info(P));
+        }
+        else
+        {
+            PrintLevel(left(P), N - 1);
+            PrintLevel(right(P), N - 1);
+        }
+    }
+}
 
 /*function GetDaunTerkiri(bintree P)-> infotype
 {mengembalikan nilai info daun terkiri, bila tidak ada, hasilnya '#' }*/
-infotype GetDaunTerkiri(bintree P);
+infotype GetDaunTerkiri(bintree P)
+{
+    // kamus lokal
+    // algoritma
+    if (P == NIL)
+    {
+        return '#';
+    }
+    else if (IsDaun(P))
+    {
+        return info(P);
+    }
+    else
+    {
+        return GetDaunTerkiri(left(P));
+    }
+}
 
 /*function FrekuensiX(P:bintree, X:infotype) -> real */
 /*{ mengembalikan rasio kemunculan X dibandingkan ukuran bintree P }*/
-float FrekuensiX(bintree P, infotype X);
+float FrekuensiX(bintree P, infotype X)
+{
+    // kamus lokal
+    int countX;
+    int total;
+    float ratio;
+    // algoritma
+    total = NbElm(P);
+    if (total == 0)
+    {
+        return 0.0;
+    }
+    else
+    {
+        countX = CountX(P, X);
+        ratio = (float)countX / (float)total;
+        return ratio;
+    }
+}
 
 /*function CountVocal(P:bintree) -> integer */
 /*{ mengembalikan banyaknya kemunculan huruf vokal dalam bintree P}*/
-int CountVocal(bintree P);
+int CountVocal(bintree P)
+{
+    // kamus lokal
+    int count;
+    char c;
+    // algoritma
+    if (P == NIL)
+    {
+        return 0;
+    }
+    else
+    {
+        c = info(P);
+        if (c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U' ||
+            c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u')
+        {
+            count = 1 + CountVocal(left(P)) + CountVocal(right(P));
+        }
+        else
+        {
+            count = CountVocal(left(P)) + CountVocal(right(P));
+        }
+        return count;
+    }
+}
 
 /*procedure PrintVocal( input P:bintree) */
 /*{ I.S.: P terdefinisi; F.S.: -
 Proses: menampilkan semua huruf vokal dalam bintree P}*/
-void PrintVocal(bintree P);
+void PrintVocal(bintree P)
+{
+    // kamus lokal
+    char c;
+    // algoritma
+    if (P != NIL)
+    {
+        c = info(P);
+        if (c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U' ||
+            c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u')
+        {
+            printf("%c ", c);
+        }
+        PrintVocal(left(P));
+        PrintVocal(right(P));
+    }
+}
 
 /*function CountConsonant(P:bintree) -> integer */
 /*{ mengembalikan banyaknya kemunculan huruf konsonan dalam bintree L}*/
-int CountConsonant(bintree P);
+int CountConsonant(bintree P)
+{
+    // kamus lokal
+    int count;
+    char c;
+    // algoritma
+    if (P == NIL)
+    {
+        return 0;
+    }
+    else
+    {
+        c = info(P);
+        if ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z'))
+        {
+            if (!(c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U' ||
+                  c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u'))
+            {
+                count = 1 + CountConsonant(left(P)) + CountConsonant(right(P));
+            }
+            else
+            {
+                count = CountConsonant(left(P)) + CountConsonant(right(P));
+            }
+        }
+        else
+        {
+            count = CountConsonant(left(P)) + CountConsonant(right(P));
+        }
+        return count;
+    }
+}
 
 /*procedure PrintConsonant( input P:bintree) */
 /*{ I.S.: P terdefinisi; F.S.: -
 Proses: menampilkan semua huruf konsonan dalam bintree P}*/
-void PrintConsonant(bintree P);
+void PrintConsonant(bintree P)
+{
+    // kamus lokal
+    char c;
+    // algoritma
+    if (P != NIL)
+    {
+        c = info(P);
+        if ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z'))
+        {
+            if (!(c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U' ||
+                  c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u'))
+            {
+                printf("%c ", c);
+            }
+        }
+        PrintConsonant(left(P));
+        PrintConsonant(right(P));
+    }
+}
 
 /*function Modus(P:bintree) -> character */
 /*{ mengembalikan huruf yang paling banyak muncul dalam bintree L}*/
-char Modus(bintree P);
+char Modus(bintree P)
+{
+    // Kamus lokal
+    infotype temp;
+    int Count;
+    char modus;
+
+    // algoritma
+    if (!IsEmptyTree(P))
+    {
+        temp = info(P);
+        Count = CountX(P, temp);
+        modus = temp;
+        // Cek modus dari kiri
+        if (!IsEmptyTree(GetLeft(P)))
+        {
+            temp = Modus(GetLeft(P));
+            if (temp != '#')
+            {
+                if (CountX(P, temp) > Count)
+                {
+                    modus = temp;
+                    Count = CountX(P, temp);
+                }
+            }
+        }
+        // Cek modus dari kanan
+        if (!IsEmptyTree(GetRight(P)))
+        {
+            temp = Modus(GetRight(P));
+            if (temp != '#')
+            {
+                if (CountX(P, temp) > Count)
+                {
+                    modus = temp;
+                    Count = CountX(P, temp);
+                }
+            }
+        }
+        if (Count == 1)
+        {
+            return '#';
+        }
+
+        return modus;
+    }
+    else
+    {
+        return '#';
+    }
+}
